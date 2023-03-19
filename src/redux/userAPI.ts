fae7f402336d55f2ea1cf72790349324c466a500
Khaled <khalegjr@gmail.com>
2023-03-19 13:15:12 -0300

    Cria os slices da api

---
+++ src/redux/userAPI.ts
@@ -0,0 +1,91 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IMutateUser, IUser, IUserResponse } from "./types";
+
+const BASEURL = "https://fakestoreapi.com/users";
+
+export const userAPI = createApi({
+  reducerPath: "userAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: BASEURL }),
+  tagTypes: ["Users"],
+  endpoints: (builder) => ({
+    createUser: builder.mutation<IUserResponse, IMutateUser>({
+      query(user) {
+        return {
+          url: "/",
+          method: "POST",
+          credentials: "include",
+          body: user,
+        };
+      },
+      invalidatesTags: [{ type: "Users", id: "LIST" }],
+      transformResponse: (result: { user: IUserResponse }) => result.user,
+    }),
+    updateUser: builder.mutation<
+      IUserResponse,
+      { id: string; user: IMutateUser }
+    >({
+      query({ id, user }) {
+        return {
+          url: `/${id}`,
+          method: "PATCH",
+          credentials: "include",
+          body: user,
+        };
+      },
+      invalidatesTags: (result, error, { id }) =>
+        result
+          ? [
+              { type: "Users", id },
+              { type: "Users", id: "LIST" },
+            ]
+          : [{ type: "Users", id: "LIST" }],
+      transformResponse: (response: { user: IUserResponse }) => response.user,
+    }),
+    getUser: builder.query<IUserResponse, string>({
+      query(id) {
+        return {
+          url: `/${id}`,
+          credentials: "include",
+        };
+      },
+      providesTags: (result, error, id) => [{ type: "Users", id }],
+    }),
+    getAllUsers: builder.query<IUser[], { page: number; limit: number }>({
+      query({ page, limit }) {
+        return {
+          url: `/?page=${page}&limit=${limit}`,
+          credentials: "include",
+        };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({
+                type: "Users" as const,
+                id,
+              })),
+              { type: "Users", id: "LIST" },
+            ]
+          : [{ type: "Users", id: "LIST" }],
+      transformResponse: (results: { users: IUser[] }) => results.users,
+      keepUnusedDataFor: 5,
+    }),
+    deleteUser: builder.mutation<IUserResponse, string>({
+      query(id) {
+        return {
+          url: `/${id}`,
+          method: "DELETE",
+          credentials: "include",
+        };
+      },
+      invalidatesTags: [{ type: "Users", id: "LIST" }],
+    }),
+  }),
+});
+
+export const {
+  useCreateUserMutation,
+  useDeleteUserMutation,
+  useUpdateUserMutation,
+  useGetAllUsersQuery,
+} = userAPI;
